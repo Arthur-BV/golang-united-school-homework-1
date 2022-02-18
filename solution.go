f7cb6a8c4a42378c565dd13a444dbec347d18779
Arthur Babayan <arthur.babayan@gmail.com>
2022-02-18 20:14:08 +0400

    My Solution for task, understanding using emoji

--- solution.go
+++ solution.go
@@ -3,7 +3,7 @@ package solution
 import Str "github.com/kyokomi/emoji/v2"
 
 func GetMessage() string {
-	str := Str.Sprint("Hello 🗺️!") // This is a comment
+	str := Str.Sprint("Hello :world_map:!") // This is a comment
 
 	return str
 }
